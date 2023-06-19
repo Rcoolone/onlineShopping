5cd0611f1ba4b67d81510d402a7c363eae220980
Rcoolone <Rcoolone@users.noreply.github.com>
2023-06-18 19:46:45 -0700

    Add helloWorld controller

---
+++ src/main/java/com/rcoolone/onlineshopping/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.rcoolone.onlineshopping.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/abcd/{text}")
+    public String helloWorld(@PathVariable("text") String abcd) {
+        return "your input is:" + abcd;
+    }
+}
